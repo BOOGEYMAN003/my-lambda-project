2168d02949c7d2f7ec746e30ce0ce99b7f80d9e8
BOOGEYMAN003 <143250767+BOOGEYMAN003@users.noreply.github.com>
2025-12-06 00:54:57 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,6 @@ def lambda_handler(event, context):
     # Your logic goes here
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from the updated Lambda!')
+        'body': json.dumps('Hello from git the updated Lambda!')
+
     }
